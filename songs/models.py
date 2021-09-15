91965eb57e3392672cb8c879e6bdd980404ab442
Ranon Larpcharern <ranonl@berkeley.edu>
2021-09-15 16:34:19 -0700

    able to create new songs and update songs

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length = 150)
+    artist = models.CharField(max_length = 150)
+    album = models.CharField(max_length = 150)
+    queue_position = models.IntegerField()
+
+    class Meta:
+        ordering = ['-queue_position']
+    
+    def __str__(self):
+        return f'{self.title} {self.artist} {self.album} {self.queue_position}'
+    
